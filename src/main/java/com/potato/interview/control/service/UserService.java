d90d43608ff42b02900b40fdbc4792783e81df47
k.potoczny <kamillo1493@gmail.com>
2022-05-13 22:17:51 +0200

    Removed unused import

--- src/main/java/com/potato/interview/control/service/UserService.java
+++ src/main/java/com/potato/interview/control/service/UserService.java
@@ -1,16 +1,12 @@
 package com.potato.interview.control.service;
 
-import com.potato.interview.boundary.dto.CardDto;
 import com.potato.interview.boundary.exception.UserNotFoundException;
-import com.potato.interview.control.repository.CardRepository;
 import com.potato.interview.control.repository.UserRepository;
 import com.potato.interview.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.function.Supplier;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
