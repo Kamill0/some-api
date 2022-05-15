7a776291db6e6f7992feabd969c8c4783ce02581
k.potoczny <kamillo1493@gmail.com>
2022-05-15 15:15:41 +0200

    Added validation for dto

--- src/main/java/com/potato/interview/boundary/dto/CardCreateDto.java
+++ src/main/java/com/potato/interview/boundary/dto/CardCreateDto.java
@@ -8,6 +8,7 @@ import lombok.With;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import java.time.LocalDate;
 
 @Data
@@ -19,6 +20,7 @@ public class CardCreateDto {
 
     @Schema(description = "Card number")
     @NotNull
+    @Pattern(regexp = "^\\d{16}$", message = "Card number must consist of 16 digits")
     private String number;
 
     @Schema(description = "Card expiration date")
